a5d48d937e346a51b354e4e59f547a5c99388bc2
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-03 02:43:33 +0530

    Wellcome To Line Comparison Program On Master Branch

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.Math;
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println(" Wellcome To Line Comparison Computation Program On Master Branch ");
+		
+
+	}
+
+}
